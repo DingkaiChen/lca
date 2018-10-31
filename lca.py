822db7c0dd1ef8618e80527d811ed43cc9073901
dkchen <jimmyraikko@sina.com>
2018-10-31 11:17:27 +0800

    to print

--- lca.py
+++ lca.py
@@ -1,8 +1,8 @@
 from app import create_app,db
-from app.models import User,Rawmaterial
+from app.models import User,Role,Rawmaterial
 
 app=create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-	return {'db':db,'User':User,'Rawmaterial':Rawmaterial}
+	return {'db':db,'User':User, 'Role':Role, 'Rawmaterial':Rawmaterial}
